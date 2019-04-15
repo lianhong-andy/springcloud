4cfd0c74b78bfd0bd2d81d1a7f606b819ac6dea9
andy <andy_lian@sina.cn>
2019-04-16 00:38:58 +0800

    origin/master springcloud quick start

---
+++ user_service/src/main/resources/user.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.2 
+MySQL - 5.5.27 : Database - springboot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`springboot` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `springboot`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(64) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  `name` varchar(64) NOT NULL DEFAULT '',
+  `age` int(3) DEFAULT NULL,
+  `sex` smallint(1) NOT NULL DEFAULT '1',
+  `birthday` date DEFAULT NULL,
+  `created` date DEFAULT NULL,
+  `updated` date DEFAULT NULL,
+  `note` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`user_name`,`password`,`name`,`age`,`sex`,`birthday`,`created`,`updated`,`note`) values (1,'liuyan','123456','柳岩',21,2,'1995-08-08','2014-09-20','2014-09-20',NULL),(2,'zhangsan','123456','张三',NULL,1,'1995-08-08','1995-08-08','1995-08-08',NULL),(3,'zhangsan','123456','张三',NULL,1,NULL,NULL,NULL,NULL),(4,'lisi','123456','lisi',NULL,1,NULL,NULL,NULL,NULL),(5,'lisi','123456','lisi',NULL,1,NULL,NULL,NULL,NULL),(6,'lisi','123456','lisi',NULL,1,NULL,NULL,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
